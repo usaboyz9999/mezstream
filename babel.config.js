fcf296d803905f04c7e2f5c6fa5d8183ba1071de
usaboyz9999 <usaboyz9999@gmail.com>
2026-03-20 06:32:12 +0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -5,3 +5,22 @@ module.exports = function(api) {
     plugins: ['react-native-reanimated/plugin'],
   };
 };
+```
+
+---
+
+## 📄 `.gitignore`
+```
+node_modules/
+.expo/
+dist/
+npm-debug.*
+*.jks
+*.p8
+*.p12
+*.key
+*.mobileprovision
+*.orig.*
+web-build/
+android/
+ios/
